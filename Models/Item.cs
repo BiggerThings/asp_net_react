2cabf47d1b3ef85dc6cb419c936e1cc5c66196dc
BiggerThings <nnsbiggerthings@gmail.com>
2025-07-11 07:24:56 +0200

    Updated Item model: made the title field required

--- Models/Item.cs
+++ Models/Item.cs
@@ -6,6 +6,6 @@ namespace MyApp.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
+        public required string Title { get; set; }
     }
 }
