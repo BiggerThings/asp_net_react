6680c3bde25bcdf1b40bd155fe228833c88d770a
BiggerThings <nnsbiggerthings@gmail.com>
2025-07-25 06:49:51 +0200

    Added button with click event

--- ClientApp/src/components/MyCompo.tsx
+++ ClientApp/src/components/MyCompo.tsx
@@ -1,8 +1,13 @@
 function MyCompo({ name, age }: { name: string; age: number }) {
+  const handleClick = () => {
+    alert(`Hello ${name}`);
+  };
+  
   return (
     <div>
       <h2>My Component Title</h2>
       <p>Hello there, {name}. You are {age} years old.</p>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
