c75a6c4730b09fe107895d319cd3bce71e5f0885
BiggerThings <nnsbiggerthings@gmail.com>
2025-07-25 07:02:12 +0200

    Added basic tailwind styling to button

--- ClientApp/src/components/MyCompo.tsx
+++ ClientApp/src/components/MyCompo.tsx
@@ -7,7 +7,7 @@ function MyCompo({ name, age }: { name: string; age: number }) {
     <div>
       <h2>My Component Title</h2>
       <p>Hello there, {name}. You are {age} years old.</p>
-      <button onClick={handleClick}>Click Me</button>
+      <button className="mt-3 px-8 py-2 rounded-sm border-solid bg-gray-300 text-white font-bold cursor-pointer"  onClick={handleClick}>Handle Event</button>
     </div>
   );
 }
