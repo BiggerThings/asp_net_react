7446fe203ba432b02c75f9ec733a5f9f615e4917
BiggerThings <nnsbiggerthings@gmail.com>
2025-07-25 07:06:36 +0200

    Added state to update button title on click event

--- ClientApp/src/components/MyCompo.tsx
+++ ClientApp/src/components/MyCompo.tsx
@@ -1,13 +1,18 @@
+import { useState } from 'react';
+
 function MyCompo({ name, age }: { name: string; age: number }) {
+  const [buttonClicked, setButtonClicked] = useState("Not clicked yet");
+  
   const handleClick = () => {
     alert(`Hello ${name}`);
+    setButtonClicked("Button clicked!");
   };
   
   return (
     <div>
       <h2>My Component Title</h2>
       <p>Hello there, {name}. You are {age} years old.</p>
-      <button className="mt-3 px-8 py-2 rounded-sm border-solid bg-gray-300 text-white font-bold cursor-pointer"  onClick={handleClick}>Handle Event</button>
+      <button className="mt-3 px-8 py-2 rounded-sm border-solid bg-gray-300 text-white font-bold cursor-pointer"  onClick={handleClick}>{buttonClicked}</button>
     </div>
   );
 }
