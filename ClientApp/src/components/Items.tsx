5f65f52dfbccf39206ff82d7f38d9bb1e89f78bf
BiggerThings <nnsbiggerthings@gmail.com>
2025-08-15 07:35:55 +0200

    Added a new for input to handle Post Method of Items DB to add new items

--- ClientApp/src/components/Items.tsx
+++ ClientApp/src/components/Items.tsx
@@ -3,6 +3,23 @@ import axios from 'axios';
 
 const Items = () => {
     const [players, setPlayers] = useState([]);
+    const [newPlayer, setNewPlayer] = useState({ title: ''});
+
+    const handleChange = (e) => {
+        setNewPlayer({ title: e.target.value });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!newPlayer.title) return;
+
+        const response = await axios.post('http://localhost:5238/api/Items', newPlayer);
+
+        if (response.status === 200 || response.status === 204) {
+            alert('Pirate added successfully!');
+            setNewPlayer({ title: '' });
+        }
+    }
 
     useEffect(() => {
         const getPlayers = async () => {
@@ -15,7 +32,21 @@ const Items = () => {
     return (
     <div className='mt-10'>
       <h3 className='text-lg font-semibold mb-2'>Items List</h3>
-      
+
+      <form>
+        <input 
+          type="text" 
+          value={newPlayer.title} 
+          onChange={handleChange} 
+          placeholder="Add new item"
+          className="border p-2 rounded"
+        />
+        <input type="submit" value="Add"
+         className="text-white bg-blue-500 border p-2 rounded"
+         onClick={handleSubmit}
+        />
+      </form>
+
       <ul>
         {players && players.map((player) => (
             <li key={player.id} className='mb-1'>
