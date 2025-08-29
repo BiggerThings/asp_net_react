16f7244a4996d1b2f84d9c376de94994aa1e55d1
BiggerThings <nnsbiggerthings@gmail.com>
2025-08-29 06:54:22 +0200

    Migrated the Item component to live inside the Items Component

--- ClientApp/src/components/Items.tsx
+++ ClientApp/src/components/Items.tsx
@@ -1,21 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Item from './Item';
+import axios from 'axios'; 
+import { Button } from "@/components/ui/button"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Input } from "@/components/ui/input"
 
 const Items = () => {
     const [players, setPlayers] = useState([]);
     const [newPlayer, setNewPlayer] = useState({ title: ''});
+    const [titleToUpdate, setTitleToUpdate] = useState('');
 
-    const handleChange = (e: React.FormEvent<HTMLFormElement>) => {
-        setNewPlayer({ title: e.target.value });
+    const handlePopUpItemInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTitleToUpdate(e.target.value);
     }
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!newPlayer.title) return;
+    const handlePopUpItemClick = (id: number, text: string) => {
+      alert('Update item button clicked! at ' + id + ' with text: ' + text);
+      setTitleToUpdate('');
+    }
 
-        await axios.post('http://localhost:5238/api/Items', newPlayer);
-        setNewPlayer({ title: '' });
+    const handleDeleteButtonClick = (id: number) => {
+      alert('Delete item button clicked! at ' + id);
+    }
+
+    const handleNewPlayerChange = (e: React.FormEvent<HTMLFormElement>) => {
+      setNewPlayer({ title: e.target.value });
+    }
+
+    const handleNewPlayerSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!newPlayer.title) return;
+      await axios.post('http://localhost:5238/api/Items', newPlayer);
+      setNewPlayer({ title: '' });
     }
 
     useEffect(() => {
@@ -34,20 +53,35 @@ const Items = () => {
         <input 
           type="text" 
           value={newPlayer.title} 
-          onChange={handleChange} 
+          onChange={handleNewPlayerChange} 
           placeholder="Add new item"
           className="border p-2 rounded w-80"
         />
         <input type="submit" value="Add"
          className="text-white bg-blue-500 border p-2 rounded ml-2"
-         onClick={handleSubmit}
+         onClick={handleNewPlayerSubmit}
         />
       </form>
 
       <ul>
         {players && players.map((player) => (
             <li key={player.id} className='mb-1'>
-                <Item item={player} />
+                {/* <Item item={player} /> */}
+                <div className='flex items-center'>
+                  <div className='w-1/6'>{player.title}</div>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                        <Button variant="outline" className="bg-blue-500 text-white">Update</Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-80 ml-10">
+                        <div className='flex w-full max-w-sm items-center gap-2 mb-6'>
+                            <Input type="text" placeholder="Add Title" onChange={handlePopUpItemInputChange} />
+                            <Button variant="outline" className="bg-blue-500 text-white" onClick={() => handlePopUpItemClick(player.id, titleToUpdate)}>Update</Button>
+                        </div>
+                    </PopoverContent>
+                  </Popover>
+                  <Button variant="destructive" className="ml-2" onClick={() => handleDeleteButtonClick(player.id)}>Delete</Button>
+                </div>
             </li>
         ))}
       </ul>
